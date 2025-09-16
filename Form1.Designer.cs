44ab09ba457d6eecf066d99cd96d36ef6d5b506d
chernishovasasha <chern.sh2005@gmail.com>
2025-09-16 17:15:18 +0300

    load

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace T2_task2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messagesTB = new System.Windows.Forms.RichTextBox();
+            this.mSendBox = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.nameBtn = new System.Windows.Forms.Button();
+            this.nameTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // messagesTB
+            // 
+            this.messagesTB.Location = new System.Drawing.Point(12, 24);
+            this.messagesTB.Name = "messagesTB";
+            this.messagesTB.ReadOnly = true;
+            this.messagesTB.Size = new System.Drawing.Size(382, 402);
+            this.messagesTB.TabIndex = 1;
+            this.messagesTB.Text = "";
+            // 
+            // mSendBox
+            // 
+            this.mSendBox.ForeColor = System.Drawing.Color.DimGray;
+            this.mSendBox.Location = new System.Drawing.Point(434, 256);
+            this.mSendBox.Name = "mSendBox";
+            this.mSendBox.Size = new System.Drawing.Size(230, 22);
+            this.mSendBox.TabIndex = 3;
+            this.mSendBox.Text = "Написати...";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.BackColor = System.Drawing.Color.DarkKhaki;
+            this.sendBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sendBtn.Location = new System.Drawing.Point(434, 294);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(230, 48);
+            this.sendBtn.TabIndex = 4;
+            this.sendBtn.Text = "Надіслати";
+            this.sendBtn.UseVisualStyleBackColor = false;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // nameBtn
+            // 
+            this.nameBtn.BackColor = System.Drawing.Color.SkyBlue;
+            this.nameBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nameBtn.Location = new System.Drawing.Point(434, 138);
+            this.nameBtn.Name = "nameBtn";
+            this.nameBtn.Size = new System.Drawing.Size(230, 48);
+            this.nameBtn.TabIndex = 6;
+            this.nameBtn.Text = "Змінити ім\'я";
+            this.nameBtn.UseVisualStyleBackColor = false;
+            this.nameBtn.Click += new System.EventHandler(this.nameBtn_Click);
+            // 
+            // nameTB
+            // 
+            this.nameTB.ForeColor = System.Drawing.Color.DimGray;
+            this.nameTB.Location = new System.Drawing.Point(434, 100);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(230, 22);
+            this.nameTB.TabIndex = 5;
+            this.nameTB.Text = "Ім\'я...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.Controls.Add(this.nameBtn);
+            this.Controls.Add(this.nameTB);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.mSendBox);
+            this.Controls.Add(this.messagesTB);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox messagesTB;
+        private System.Windows.Forms.TextBox mSendBox;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Button nameBtn;
+        private System.Windows.Forms.TextBox nameTB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T2_task2._1
+{
+    public partial class Form1 : Form
+    {
+        string name;
+        private TcpClient client;
+        private NetworkStream stream;
+        private const int PORT = 12345;
+        private const string IP = "127.0.0.1";
+        public Form1()
+        {
+            InitializeComponent();
+            client = new TcpClient(IP, PORT);
+            stream = client.GetStream();
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            SendMessages(name);
+        }
+
+        private void nameBtn_Click(object sender, EventArgs e)
+        {
+            messagesTB.AppendText($"{name} змінив ім'я на {nameTB.Text}");
+            name = nameTB.Text;
+        }
+
+        public void SendMessages(string name)
+        {
+            try
+            {
+                new Thread(ReceiveMessages).Start();
+                while (true)
+                {
+                    string message = mSendBox.Text;
+                    byte[] buffer = Encoding.UTF8.GetBytes($"{name}: {message}");
+                    stream.Write(buffer, 0, buffer.Length);
+                    messagesTB.AppendText($"Я: {message}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                messagesTB.AppendText($"Помилка надсилання повідомлення: {ex.Message}");
+            }
+
+        }
+
+        public void ReceiveMessages()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = 0;
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        messagesTB.AppendText(message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    messagesTB.AppendText($"Помилка надсилання повідомлення: {ex.Message}");
+                    break;
+                }
+            }
+        }
+    }
+}
